2974a03951ef5ba9c8e0510ea113968bd3382e2c
david3464 <51375293+david3464@users.noreply.github.com>
2019-10-30 15:29:55 -0400

    1

--- 1.4PA-AutoCircleRotate/public/javascripts/1PhotoAnimation/4AutoCircleRotate/animation.js
+++ 1.4PA-AutoCircleRotate/public/javascripts/1PhotoAnimation/4AutoCircleRotate/animation.js
@@ -1,22 +1,23 @@
-
-window.onload = function() { 
-  var photosDom = document.getElementById('photos');
-  //获取图片数组
-  var images = photosDom.getElementsByTagName('img'); 
-  //获取图片数量
-  var len = images.length;        
-  //计算每张图片按Y轴旋转的角度 
-  var deg = Math.floor(360 / len);                    
-  for(var i = 0; i < len;i++){
-      images[i].style = 'transform : rotateY(' + deg * i + 'deg)'; //deg前面不要加空格
-      for(var i = 0; i < len;i++){
-        images[i].style = 'transform : rotateY(' + deg * i + 'deg) translateZ(380px)'; //deg前面不要加空格
-      }
-  }
-
-  var x = 0;
-  setInterval(function(){
-    photosDom.style.transform = "rotateX(-10deg) rotateY("+ (x++) * 0.2 +"deg)";
-  },30);
-
+var _$_508f=["\x6F\x6E\x6C\x6F\x61\x64\xA0","\x70\x68\x6F\x74\x6F\x73","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x69\x6D\x67","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65","\x6C\x65\x6E\x67\x74\x68","\x66\x6C\x6F\x6F\x72","\x73\x74\x79\x6C\x65","\x74\x72\x61\x6E\x73\x66\x6F\x72\x6D\x20\x3A\x20\x72\x6F\x74\x61\x74\x65\x59\x28","\x64\x65\x67\x29","\x64\x65\x67\x29\x20\x74\x72\x61\x6E\x73\x6C\x61\x74\x65\x5A\x28\x33\x38\x30\x70\x78\x29","\x74\x72\x61\x6E\x73\x66\x6F\x72\x6D","\x72\x6F\x74\x61\x74\x65\x58\x28\x2D\x31\x30\x64\x65\x67\x29\x20\x72\x6F\x74\x61\x74\x65\x59\x28"];
+window[_$_508f[0]]= function()
+{
+	var _0x25248=document[_$_508f[2]](_$_508f[1]);//2
+	var _0x251D2=_0x25248[_$_508f[4]](_$_508f[3]);//4
+	var _0x2520D=_0x251D2[_$_508f[5]];//6
+	var _0x2515C=Math[_$_508f[6]](360/ _0x2520D);//8
+	for(var _0x25197=0;_0x25197< _0x2520D;_0x25197++)
+	{
+		_0x251D2[_0x25197][_$_508f[7]]= _$_508f[8]+ _0x2515C* _0x25197+ _$_508f[9];for(var _0x25197=0;_0x25197< _0x2520D;_0x25197++)
+		{
+			_0x251D2[_0x25197][_$_508f[7]]= _$_508f[8]+ _0x2515C* _0x25197+ _$_508f[10]
+		}
+		
+	}
+	//9
+	var _0x25283=0;//16
+	setInterval(function()
+	{
+		_0x25248[_$_508f[7]][_$_508f[11]]= _$_508f[12]+ (_0x25283++)*  0.2+ _$_508f[9]
+	}
+	,30)
 }
